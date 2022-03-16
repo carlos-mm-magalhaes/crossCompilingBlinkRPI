a26474e8eeca11469d6d0487104ea3eb8eda5e95
Carlos Magalhaes <carlos.marcone.magalhaes@gmail.com>
2022-03-16 19:01:49 +0100

    uncomment cross compiler declaration in CMakeLists

--- toolchain-rpi.cmake
+++ toolchain-rpi.cmake
@@ -5,8 +5,8 @@ set(CMAKE_SYSTEM_VERSION 1)
 # set(CMAKE_SYSTEM_PROCESSOR armv6)
 
 # Define the cross compiler locations
-set(tools ./compilers/cross-pi-gcc-8.3.0-0)
-set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/bin/arm-linux-gnueabihf-g++)
+# set(tools ./compilers/cross-pi-gcc-8.3.0-0)
+# set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabihf-gcc)
+# set(CMAKE_CXX_COMPILER ${tools}/bin/arm-linux-gnueabihf-g++)
 
 add_definitions(-Wall)
